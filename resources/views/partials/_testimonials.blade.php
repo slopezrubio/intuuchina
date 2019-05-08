3041a4ef63985e9c2319922efb5ca1bf2253f9df
kalvo19 <recmanvideos@gmail.com>
2019-05-08 17:41:15 +0200

    [ADD] Formulario del footer con switch

--- resources/views/partials/_testimonials.blade.php
+++ resources/views/partials/_testimonials.blade.php
@@ -104,24 +104,24 @@
 
         </div>
 
-        <div class="testim">
-            <img class="linkedinpic"src="https://media.licdn.com/dms/image/C5103AQG35Aiy3GHRMw/profile-displayphoto-shrink_800_800/0?e=1541635200&v=beta&t=P-ELQEc73TATcHP02SaxKg1IBA8nBYVTu6S15YZj9sA" alt="profilepic">
-            <h3>Maria Alejandra Sanabria Aguilar</h3>
-            <h4>BD Executive at SIP Project Management</h4>
-            <!--<h5>September 25, 2016, Maria Alejandra was a client of IntuuChina</h5>-->
-            <p>"Fernando is a focused and intelligent person that develops a very successful company"</p>
-            <a href="https://www.linkedin.com/in/mariaalejandrasanabriaaguilar/"><i class="fab fa-linkedin"></i></a>
+{{--        <div class="testim">--}}
+{{--            <img class="linkedinpic"src="https://media.licdn.com/dms/image/C5103AQG35Aiy3GHRMw/profile-displayphoto-shrink_800_800/0?e=1541635200&v=beta&t=P-ELQEc73TATcHP02SaxKg1IBA8nBYVTu6S15YZj9sA" alt="profilepic">--}}
+{{--            <h3>Maria Alejandra Sanabria Aguilar</h3>--}}
+{{--            <h4>BD Executive at SIP Project Management</h4>--}}
+{{--            <!--<h5>September 25, 2016, Maria Alejandra was a client of IntuuChina</h5>-->--}}
+{{--            <p>"Fernando is a focused and intelligent person that develops a very successful company"</p>--}}
+{{--            <a href="https://www.linkedin.com/in/mariaalejandrasanabriaaguilar/"><i class="fab fa-linkedin"></i></a>--}}
 
-        </div>
+{{--        </div>--}}
 
-        <div class="testim">
-            <img class="linkedinpic"src="https://media.licdn.com/dms/image/C4E03AQGeaHEYiv8eRA/profile-displayphoto-shrink_200_200/0?e=1545264000&v=beta&t=c1zKCqNdjU-6O0uTNEGjBrB41u_J7T7B8AOh5fHQTOs" alt="profilepic">
-            <h3>Mario Juárez Camacho</h3>
-            <h4>Digital Analyst & Strategist</h4>
-            <!-- <h5>September 5, 2016, Mario was a client of IntuuChina</h5> -->
-            <p>"IntuuChina is a great one-stop shop that really makes your life easier"</p>
-            <a href="https://www.linkedin.com/in/mario-juarez/"><i class="fab fa-linkedin"></i></a>
+{{--        <div class="testim">--}}
+{{--            <img class="linkedinpic"src="https://media.licdn.com/dms/image/C4E03AQGeaHEYiv8eRA/profile-displayphoto-shrink_200_200/0?e=1545264000&v=beta&t=c1zKCqNdjU-6O0uTNEGjBrB41u_J7T7B8AOh5fHQTOs" alt="profilepic">--}}
+{{--            <h3>Mario Juárez Camacho</h3>--}}
+{{--            <h4>Digital Analyst & Strategist</h4>--}}
+{{--            <!-- <h5>September 5, 2016, Mario was a client of IntuuChina</h5> -->--}}
+{{--            <p>"IntuuChina is a great one-stop shop that really makes your life easier"</p>--}}
+{{--            <a href="https://www.linkedin.com/in/mario-juarez/"><i class="fab fa-linkedin"></i></a>--}}
 
-        </div>
+{{--        </div>--}}
     </div>
 </section>
