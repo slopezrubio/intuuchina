cc5c4a00826819984f7c50f7953d735b342ab77b
kalvo19 <recmanvideos@gmail.com>
2019-06-19 00:42:41 +0200

    [UPDATE] Pruebas de cobertura de test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @test
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+
+}
