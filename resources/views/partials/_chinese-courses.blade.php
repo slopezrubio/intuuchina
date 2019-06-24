10ac85f80f2d6811d641676d5c1fa65cc22ae762
kalvo19 <recmanvideos@gmail.com>
2019-06-24 11:06:51 +0200

    [INFO] Commit para poder hacer PULL desde otro ordenador

--- resources/views/partials/_chinese-courses.blade.php
+++ resources/views/partials/_chinese-courses.blade.php
@@ -29,7 +29,7 @@
 <section class="course-information" id="bg-classroom-course">
     <div class="course-information_box">
         <p><span>Desde</span><span class="price-tag">499<i class="euro">€</i> </span></p>
-        <p><span>( Estancia mínima de 1 mes )</span></p>
+        <p><span class="advice">( Estancia mínima de 1 mes )</span></p>
     </div>
     <div class="course-information_box"></div>
 </section>
