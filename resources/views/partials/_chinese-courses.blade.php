07dc615a688ba9c103d5fbb00d5a866b47c57e27
kalvo19 <recmanvideos@gmail.com>
2019-07-09 10:50:18 +0200

    [ADD] Comprobando peticiones AJAX para la página de Aprende Chino

--- resources/views/partials/_chinese-courses.blade.php
+++ resources/views/partials/_chinese-courses.blade.php
@@ -26,10 +26,4 @@
         <a href="#"><span>Online</span></a>
     </div>
 </section>
-<section class="course-information" id="bg-classroom-course">
-    <div class="course-information_box">
-        <p><span>Desde</span><span class="price-tag">499<i class="euro">€</i> </span></p>
-        <p><span class="advice">( Estancia mínima de 1 mes )</span></p>
-    </div>
-    <div class="course-information_box"></div>
-</section>
+@include('partials._price-course-info')
---
+++ resources/views/partials/_price-course-info.blade.php
@@ -0,0 +1,7 @@
+<section class="course-information" id="bg-classroom-course">
+    <div class="course-information_box">
+        <p><span>Desde</span><span class="price-tag">499<i class="euro">€</i> </span></p>
+        <p><span class="advice">( Estancia mínima de 1 mes )</span></p>
+    </div>
+    <div class="course-information_box"></div>
+</section>
