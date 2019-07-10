97e5de7b718f9fa4aab864c26389d6c22ec83770
kalvo19 <recmanvideos@gmail.com>
2019-07-10 21:18:17 +0200

    [UPDATE] Maquetación del formulario de registro adaptado hasta 750px de ancho

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,15 +6,17 @@
         display: none !important;
       }
     </style>
-    <header>
+    <header id="register">
+        {{--Elemento NAV--}}
         @include('partials._nav')
+
+        {{--Título de la página--}}
+        @include('partials._page-title')
     </header>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
-
+            <div class="col-md-12">
+                <div class="card extended-form">
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
@@ -67,47 +69,145 @@
                             <div class="form-group row" id="programFieldset">
                                 <label for="inputProgram" class="col-md-4 col-form-label text-md-right">Program</label>
                                 <div class="col-md-6">
-                                    <select class="custom-select" id="inputProgram" name="program">
-                                        <option value="internship" aria-selected="true" selected>Internship program</option>
-                                        <option value="inter_relocat">Internship + Relocation Program</option>
-                                        <option value="inter_housing"> Internship including housing</option>
-                                        <option value="study">Study Chinese</option>
-                                        <option value="universty">University</option>
-                                    </select>
+                                    <div class="regular-select-wrapper">
+                                        <select class="form-control" id="inputProgram" name="program">
+                                            <option value="internship" aria-selected="true" selected>Internship program</option>
+                                            <option value="inter_relocat">Internship + Relocation Program</option>
+                                            <option value="inter_housing">Internship Including Housing</option>
+                                            <option value="study">Study Chinese</option>
+                                            <option value="universty">University</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
 
                             <div class="form-group row" id="industryFieldset">
                                 <label for="inputIndustry" class="col-md-4 col-form-label text-md-right">Industry</label>
                                 <div class="col-md-6">
-                                    <label><input type="checkbox" value="finance" name="industry[]">Finance</label>
-                                    <label><input type="checkbox" value="design" name="industry[]">Design</label>
-                                    <label><input type="checkbox" value="engineering" name="industry[]">Engineering</label>
-                                    <label><input type="checkbox" value="consultant" name="industry[]">Consultant</label>
-                                    <label><input type="checkbox" value="education" name="industry[]">Education</label>
-                                    <label><input type="checkbox" value="hospitality" name="industry[]">Hospitality</label>
-                                    <label><input type="checkbox" value="it" name="industry[]">IT</label>
-                                    <label><input type="checkbox" value="legal" name="industry[]">Legal</label>
-                                    <label><input type="checkbox" value="logistic" name="industry[]">Logistic</label>
-                                    <label><input type="checkbox" value="marketing_business" name="industry[]">Marketing & Business Development</label>
-                                    <label><input type="checkbox" value="human_resources" name="industry[]">HR</label>
+                                    <div class="sw_input">
+                                        <label aria-label="finance">Finance</label>
+                                        <label for="finance" class="switch">
+                                            <input id="finance" type="checkbox" value="finance" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="design">Design</label>
+                                        <label for="design" class="switch">
+                                            <input id="design" type="checkbox" value="design" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="engineering">Engineering</label>
+                                        <label for="engineering" class="switch">
+                                            <input id="engineering" type="checkbox" value="engineering" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="consultant">Consultant</label>
+                                        <label for="consultant" class="switch">
+                                            <input id="consultant" type="checkbox" value="consultant" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="education">Education</label>
+                                        <label for="education" class="switch">
+                                            <input id="education" type="checkbox" value="education" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="hospitality">Hospitality</label>
+                                        <label for="hospitality" class="switch">
+                                            <input id="hospitality" type="checkbox" value="hospitality" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="it">IT</label>
+                                        <label for="it" class="switch">
+                                            <input id="it" type="checkbox" value="it" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="legal">Legal</label>
+                                        <label for="legal" class="switch">
+                                            <input id="legal" type="checkbox" value="legal" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="logistic">Logistic</label>
+                                        <label for="logistic" class="switch">
+                                            <input id="logistic" type="checkbox" value="logistic" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="marketing_business">Marketing & Business Development</label>
+                                        <label for="marketing_business" class="switch">
+                                            <input id="marketing_business" type="checkbox" value="marketing_business" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="hr">Human Resources</label>
+                                        <label for="hr" class="switch">
+                                            <input id="hr" type="checkbox" value="hr" name="industry[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
 
                             <div class="form-group row" id="studyFieldset">
                                 <label for="inputStudy" class="col-md-4 col-form-label text-md-right">Study Chinese</label>
                                 <div class="col-md-6">
-                                    <label><input type="checkbox" value="online" name="study[]">Online</label>
-                                    <label><input type="checkbox" value="presencial" name="study[]">Presencial</label>
+                                    <div class="sw_input">
+                                        <label aria-label="online">Online</label>
+                                        <label for="online" class="switch">
+                                            <input id="online" type="checkbox" value="online" name="study[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="presencial">Presencial</label>
+                                        <label for="presencial" class="switch">
+                                            <input id="presencial" type="checkbox" value="presencial" name="study[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
 
                             <div class="form-group row" id="universityFieldset">
                                 <label for="inputUniversity" class="col-md-4 col-form-label text-md-right">University</label>
                                 <div class="col-md-6">
-                                    <label><input type="checkbox" value="mba" name="university[]">Master of Business Administration</label>
-                                    <label><input type="checkbox" value="mib" name="university[]">Master of International Business</label>
-                                    <label><input type="checkbox" value="others" name="university[]">Others</label>
+                                    <div class="sw_input">
+                                        <label aria-label="mba">MBA (Master of Business Administration)</label>
+                                        <label for="mba" class="switch">
+                                            <input id="mba" type="checkbox" value="mba" name="university[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="mib">M. Intl. Bsns.</label>
+                                        <label for="mib" class="switch">
+                                            <input id="mib" type="checkbox" value="mib" name="university[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sw_input">
+                                        <label aria-label="other">Other</label>
+                                        <label for="other" class="switch">
+                                            <input id="other" type="checkbox" value="other" name="university[]">
+                                            <i class="checkbox_slider fas checkbox_slider--rounded"></i>
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
 
@@ -146,4 +246,7 @@
             </div>
         </div>
     </div>
+
+    {{--Elemento FOOTER--}}
+    @include('partials._footer')
 @endsection
--- resources/views/partials/_page-title.blade.php
+++ resources/views/partials/_page-title.blade.php
@@ -3,6 +3,8 @@
         @if( Request::is('/'))
             <button class="cta col-10 col-xs-10 col-sm-8 col-md-8 col-lg-4"><a href="{{ route('offers') }}">Ver ofertas<i class="fas fa-chevron-right"></i></a></button>
             <h1 class="title-card-header">Prácticas en primeras empresas del sector tecnológico</h1>
+        @elseif(Request::is('register'))
+            <h1 class="title-card-header">Regístrate</h1>
         @else
             <h1 class="title-card-header">{{ $params->title }}</h1>
         @endif
