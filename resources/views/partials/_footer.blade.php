d92000d6c30310ba47fb27ab17dcc56375723dab
kalvo19 <recmanvideos@gmail.com>
2019-07-25 00:25:02 +0200

    [ADD] Añadido los textos y maquetado estilos de la ventana modal de los términos y condiciones

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -68,56 +68,11 @@
 {{--                </div>--}}
                 <div class="col-xs-10 switch_input">
                     <label aria-label="terms">Acepto los <a href="#" data-toggle="modal" data-target="#termsAndConditionsModal">términos y condiciones</a> así como también el <a href="#" data-toggle="modal" data-target="#GDPRModal">Reglamento General de Protección de datos</a></label>
+                    @include('partials._terms-and-conditions')
 
-                    <!-- Terms and Conditions Modal -->
-                    <div class="modal fade" id="termsAndConditionsModal" tabindex="-1" role="dialog" aria-labelledby="termsAndConditionsModalTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-scrollable" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <h5 class="modal-title" id="termsAndConditionsModalTitle">Términos y condiciones</h5>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                    @include('partials._gdpr')
 
-                    <!-- GDPR Modal -->
-                    <div class="modal fade" id="GDPRModal" tabindex="-1" role="dialog" aria-labelledby="GDPRModalTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-scrollable" role="document">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                                <h5 class="modal-title" id="GDPRModalTitle">Reglamento General de Protección de datos</h5>
-                            </div>
-                        <div class="modal-body">
-                            En primer lugar le agradecemos el interés que ha mostrado por dirigirse IntuuChina Ltd. al facilitarnos sus datos e informacion.<br><br>
-
-                            Le informamos que, de conformidad con la normativa sobre protección de datos, sus datos serán objeto de tratamiento por IntuuChina Ltd. como Responsable del mismo con la finalidad de gestionar su currículum para la selección de personal. Si su perfil no se ajustase a los requisitos pertinentes en los vigentes procesos de selección procederemos a conservar sus datos para futuros procesos que sí se ajusten a su perfil, salvo que Vd. nos manifieste lo contrario.<br><br>
-
-                            Contamos con su consentimiento para el tratamiento de los datos que nos ha facilitado de forma voluntaria, libre e informada a efectos de participar en los procesos de selección de la organización.<br><br>
-
-                            Por otro lado, queremos comunicarle que no cederemos sus datos a terceros, salvo autorización expresa u obligación legal. Tampoco están previstas transferencias internacionales a otros países.<br><br>
-
-                            Podrá ejercer sus derechos de acceso, rectificación, supresión, oposición, limitación del tratamiento, portabilidad, transparencia en la información y a no ser objeto de decisiones individualizadas automatizadas (incluida la elaboración de perfiles),  comunicándolo por escrito, mediante el envío de un correo electrónico a: info@intuuchina.com<br><br>
-
-                            Para más información sobre nuestra Política de Privacidad, puede consultar el siguiente link: www.intuuchina.com<br><br>
-
-                            Sin otro particular, aprovechamos la ocasión para enviarle un cordial saludo.<br><br>
-
-                            Atentamente,<br><br>
-
-                            INTUUUCHINA Ltd<br>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                    @include('partials._privacy-policy')
 
                     <label for="terms" class="switch">
                         <input id="terms" name="terms" type="checkbox">
@@ -170,7 +125,9 @@
                     <p>Made with love &hearts; by <a href="http://factoriaf5.org"></p>
                     <img src="{{ asset('storage/images/logo_factoriaf5.png') }}" alt="Logo de factoriaF5">
                 </span>
-                <p><a href="{{ url('/privacy-policy') }}">Politica de privacidad</a> | <a href="{{ route('terms&conditions') }}">Terminos y condiciones</a></p>
+                <p>
+                    <a href="#" data-toggle="modal" data-target="#privacyPolicy">Política de privacidad</a> | <a href="#" data-toggle="modal" data-target="#termsAndConditionsModal">Terminos y condiciones</a>
+                </p>
             </div>
         </div>
     </div>
